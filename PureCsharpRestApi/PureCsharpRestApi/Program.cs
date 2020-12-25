acf78e2695171b27ced1b2b4ed11d37a46751252
Ivan Brygar <smitbmx@gmail.com>
2020-12-25 18:21:08 +0800

    Added newton json and code for deserialization.

--- PureCsharpRestApi/PureCsharpRestApi/Program.cs
+++ PureCsharpRestApi/PureCsharpRestApi/Program.cs
@@ -16,10 +16,82 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
 namespace PureCsharpRestApi
 {
+    public class PageResponse
+    {
+        public string Page { get; set; }
+
+        //[JsonProperty("per_page")]
+        [System.Runtime.Serialization.DataMember(Name = "per_page")]
+        public int PerPage { get; set; }
+
+        public int Total { get; set; }
+
+        [JsonProperty("total_pages")]
+        public int TotalPages { get; set; }
+
+        public IEnumerable<Movie> Data { get; set; }
+    }
+
+    public class Movie
+    {
+        public string Poster { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+
+        [JsonProperty("imdbID")]
+        public string ImdbId { get; set; }
+    }
+
     class Program
     {
+        public static async Task<IEnumerable<Movie>> GetMoviesAsync()
+        {
+            var movies = new List<Movie>();
+            var url = "http://jsonmock.hackerrank.com/api/movies/search/?Title=spiderman";
+            int currentPage = 1;
+            int totalPages = 0;
+            var nextUrl = $"{url}&page={currentPage}";
+
+            using (var httpClient = new HttpClient())
+            {
+                do
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(nextUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        var pageResponse = JsonConvert.DeserializeObject<PageResponse>(json);
+
+                        if (pageResponse != null && pageResponse.Data.Any())
+                        {
+                            movies.AddRange(pageResponse.Data);
+                            totalPages = pageResponse.TotalPages;
+
+                            currentPage++;
+                            nextUrl = $"{url}&page={currentPage}";
+                        }
+                        else
+                        {
+                            break; // or throw exception
+                        }
+                    }
+                    else
+                    {
+                        break; // or throw exception
+                    }
+                } while (currentPage < totalPages);
+            }
+
+            return movies;
+        }
+
         public static int getNumDraws(int year)
         {
             int res = 0;
@@ -30,6 +102,9 @@ namespace PureCsharpRestApi
 
         static void Main(string[] args)
         {
+            IEnumerable<Movie> movies = GetMoviesAsync().GetAwaiter().GetResult();
+            Console.WriteLine($"Retrieved {movies.Count()} movies.");
+
             Console.WriteLine("Hello World!");
         }
     }
