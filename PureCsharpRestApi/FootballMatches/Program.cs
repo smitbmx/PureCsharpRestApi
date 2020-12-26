617792d560dd926585865ce0aa1b14fd063daa93
Ivan Brygar <smitbmx@gmail.com>
2020-12-26 11:46:44 +0800

    Added Newton.Json dependency and classes for Json representation

--- PureCsharpRestApi/FootballMatches/Program.cs
+++ PureCsharpRestApi/FootballMatches/Program.cs
@@ -1,7 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace FootballMatches
 {
+    public class PageResponse
+    {
+        [JsonProperty("page")]
+        public string Page { get; set; }
+
+        [JsonProperty("per_page")]
+        public int PerPage { get; set; }
+
+        [JsonProperty("total")]
+        public int Total { get; set; }
+
+        [JsonProperty("total_pages")]
+        public int TotalPages { get; set; }
+
+        public IEnumerable<FootballMatch> Data { get; set; }
+    }
+
+    public class FootballMatch
+    {
+        [JsonProperty("competition")]
+        public string Competition { get; set; }
+
+        [JsonProperty("year")]
+        public int Year { get; set; }
+
+        [JsonProperty("round")]
+        public int Round { get; set; }
+
+        [JsonProperty("team1")]
+        public int Team1 { get; set; }
+
+        [JsonProperty("team2")]
+        public int Team2 { get; set; }
+
+        [JsonProperty("team1goals")]
+        public int Team1Goals { get; set; }
+
+        [JsonProperty("team2goals")]
+        public int Team2Goals { get; set; }
+    }
+
     class Program
     {
         public static int getNumDraws(int year)
