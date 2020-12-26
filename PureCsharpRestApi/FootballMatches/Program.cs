3f5a1653227485fe941f908a8cc0be6179c66eb4
Ivan Brygar <smitbmx@gmail.com>
2020-12-26 11:39:09 +0800

    Added the new project Football Matches.

---
+++ PureCsharpRestApi/FootballMatches/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FootballMatches
+{
+    class Program
+    {
+        public static int getNumDraws(int year)
+        {
+            int res = 0;
+            //https://jsonmock.hackerrank.com/api/football_matches?year=2016&page=100
+            //https://jsonmock.hackerrank.com/api/football_matches?team1goals=1&team2goals=1&year=2011&page=1
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
